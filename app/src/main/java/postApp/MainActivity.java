6c47857fdc286c680fd5f35e0061bc3133465210
adinhodovic <hodovicadin@gmail.com>
2016-10-12 15:09:07 +0200

    Just added a couple of basic stuff

--- app/src/main/java/postApp/MainActivity.java
+++ app/src/main/java/postApp/MainActivity.java
@@ -1,15 +1,14 @@
 package postApp;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import adin.postApp.R;
 
@@ -20,17 +19,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton checkmark = (ImageButton) findViewById(R.id.checkmark);
+        Button colorblue = (Button) findViewById(R.id.buttonblue);
+        Button coloryellow = (Button) findViewById(R.id.buttonyellow);
+        Button colorgreen = (Button)findViewById(R.id.buttongreen);
+        final EditText titletext = (EditText)findViewById(R.id.titletext);
+        final EditText typedtext = (EditText)findViewById(R.id.typedText);
+        final ImageView PostitImage = (ImageView)findViewById(R.id.ImageView);
+        final String[] Color = new String[1];
 
-        Button button5 = (Button) findViewById(R.id.pubtest);
-        button5.setOnClickListener(new View.OnClickListener() {
+        assert checkmark != null;
+        checkmark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Sendpostit post = new Sendpostit();
-                post.start();
+                String text = typedtext.getText().toString();
+                String title = titletext.getText().toString();
+                String urlParameters = "Hostname: codehigh ClientId: new client Topic:New topic Body: {\"Postit\": {\"Color\": \" " + Color[0] + "\",\"Title\": \"" + titletext + "\",\"Text\": \"" + typedtext + "\"}} end";
+                String myUrl = "http://codehigh.ddns.net:5000/";
+                //String myUrl = "http://localhost:5600/";
+
+                post.executePost(myUrl, urlParameters);
+            }
+        });
+
+
+        colorblue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Color[0] = "Blue";
+                PostitImage.setImageDrawable(getResources().getDrawable(R.mipmap.bluepost));
+            }
+        });
+
+        coloryellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Color[0] = "Yellow";
+                PostitImage.setImageDrawable(getResources().getDrawable(R.mipmap.yellowpost));
+            }
+        });
+
+        colorgreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Color[0] = "Green";
+                PostitImage.setImageDrawable(getResources().getDrawable(R.mipmap.greenpost));
             }
         });
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
+
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
--- app/src/main/java/postApp/Sendpostit.java
+++ app/src/main/java/postApp/Sendpostit.java
@@ -1,35 +1,56 @@
 package postApp;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttTopic;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-/**
- * Created by adinH on 2016-10-04.
- */
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
 public class Sendpostit {
 
+    public static String executePost(String targetURL, String urlParameters) {
+        HttpURLConnection connection = null;
 
-        public Sendpostit() {
-            try {
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", urlParameters);
 
-            } catch (Exception e) {
-                System.exit(1);
+
+            connection.setRequestProperty("Content-Length",
+                    Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
             }
         }
-
-        public void start() {
-            try {
-                publishPostIt();
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-
-        public void publishPostIt()  {
-            System.out.println("done");
-        }
-
     }
+}
+
+
+
+
+
