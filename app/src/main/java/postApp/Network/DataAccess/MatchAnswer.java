6931e7f2957c777b82f8d7af0979b3e9d1ec4b15
EmanuelMarcos <emnricardo@gmail.com>
2016-11-08 15:34:59 +0100

    Added network package and classes

--- app/src/main/java/postApp/Network/DataAccess/MatchAnswer.java
+++ app/src/main/java/postApp/Network/DataAccess/MatchAnswer.java
@@ -3,73 +3,73 @@ package postApp.Network.DataAccess;
 import android.os.AsyncTask;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
-public class Login {
+/**
+ * Created by Emanuel on 08/11/2016.
+ */
+
+public class MatchAnswer {
     private DBConnection conn;
     private String user;
-    private String password;
-    private boolean logedIn;
+    private String answer;
     private Connection c;
-    private String answerMatch;
     private boolean match;
 
 
-    public Login(String User, String Password) {
+    public MatchAnswer(String User, String Answer) {
         try {
             conn = new DBConnection();
             conn.execute();
             c = conn.get();
             this.user = User;
-            this.password = Password;
+            this.answer = Answer;
         } catch (Exception v) {
             System.out.println(v);
         }
 
     }
 
-    private class authenticate extends AsyncTask<Void, Void, Boolean> {
+    private class matchAnswer extends AsyncTask<Void, Void, Boolean> {
 
         protected Boolean doInBackground(Void... arg0)
         {
             try {
-                String query = "select UserID, Password from Users where UserID=? and Password=? ";
-                PreparedStatement psLogin = c.prepareStatement(query);
-                psLogin.setString(1, user);
-                psLogin.setString(2, password);
-                ResultSet rs = psLogin.executeQuery();
+                String answerQuery = "select UserID, Answer from Users where Answer=? and Answer=? ";
+                PreparedStatement psAnswer = c.prepareStatement(answerQuery);
+                psAnswer.setString(1, user);
+                psAnswer.setString(2, answer);
+                ResultSet rs = psAnswer.executeQuery();
 
                 int count = 0;
                 while (rs.next()) {
                     count++;
                 }
                 if (count == 1) {
-                    logedIn = true;
+                    match = true;
 
                 } else {
-                    logedIn = false;
+                    match = false;
                 }
 
-                psLogin.close();
+                psAnswer.close();
             } catch (Exception e) {
                 e.printStackTrace();
 
             }
-            return logedIn;
+            return match;
         }
 
     }
 
 
-    public boolean getStatus(){
+    public boolean getAnswerMatch(){
         try {
-            authenticate au = new authenticate();
-            au.execute();
-            return au.get();
+            matchAnswer ma = new matchAnswer();
+            ma.execute();
+            return ma.get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -79,7 +79,4 @@ public class Login {
     }
 
 
-
-
 }
-
