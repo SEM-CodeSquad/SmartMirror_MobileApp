44681a32cae942fd74974ff92e3995676079af74
CYuZeric <chyj1993@gmail.com>
2016-12-03 15:42:10 +0100

    Shopping list publish JsonBuilder update. Shopping list subscribe Json Parser added

--- app/src/main/java/postApp/DataHandlers/MenuHandlers/FragmentHandlers/ExternalSystems/ShoppingHandler.java
+++ app/src/main/java/postApp/DataHandlers/MenuHandlers/FragmentHandlers/ExternalSystems/ShoppingHandler.java
@@ -1,11 +1,11 @@
 package postApp.DataHandlers.MenuHandlers.FragmentHandlers.ExternalSystems;
 
-import android.bluetooth.BluetoothClass;
-
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import postApp.ActivitiesView.MenuView.FragmentViews.ExternalSystem.ShoppingView;
@@ -15,31 +15,84 @@ import postApp.Presenters.MenuPresenters.FragmentPresenters.ExternalSystems.Shop
 public class ShoppingHandler {
     ShoppingPresenter ShoppingPresenter;
     ShoppingView ShoppingView;
+
     private String message;
-    private String contentType;
-    private String content;
-    private String messageFrom;
-    private String timestamp;
-    private LinkedList<BluetoothClass.Device> deviceList;
+    private String reply;
+    private String preData;
+    private LinkedList<ShoppingList> SPLList;
 
     public ShoppingHandler(ShoppingView ShoppingView, ShoppingPresenter ShoppingPresenter) {
         this.ShoppingView = ShoppingView;
         this.ShoppingPresenter = ShoppingPresenter;
 
     }
+
     public void parseMessage(String message) {
 
         try {
             this.message = message;
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(this.message);
-            this.messageFrom = json.get("messageFrom").toString();
-            this.timestamp = json.get("timestamp").toString();
-            this.contentType = json.get("contentType").toString();
-            this.content = json.get("content").toString();
+            this.reply = json.get("reply").toString();
+            this.preData = json.get("item").toString();
+            this.SPLList = new LinkedList<>();
+            parseArray(this.SPLList, "data");
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
+
+
+    //TODO unsure if it's needed.
+//    public void parseContent() {
+//        try {
+//            JSONParser parser = new JSONParser();
+//            JSONArray jsonArray;
+//            JSONObject jso;
+//
+//            switch (getReply()) {
+//                case "Done":
+//                    jsonArray = (JSONArray) parser.parse(this.preData);
+//                    jso = (JSONObject) parser.parse(jsonArray.get(0).toString());
+//                    preData = jso.get("item").toString();
+//
+//                    break;
+//            }
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+
+    private void parseArray(LinkedList linkedList, String type) {
+        try {
+            JSONParser parser = new JSONParser();
+            JSONArray jsonArray = (JSONArray) parser.parse(this.preData);
+            String s = jsonArray.get(0).toString();
+            JSONObject jso = (JSONObject) parser.parse(s);
+            ArrayList<String> arrayList = new ArrayList<>(jso.keySet());
+            String item;
+            for (String anArrayList : arrayList) {
+                String value = jso.get(anArrayList).toString();
+                switch (type) {
+                    case "item":
+                        item = value;
+                        linkedList.add(item);
+                        break;
+                    default:
+                        System.out.println("Could Not be Parsed!");
+                        break;
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+    public String getReply(){
+        return reply;
+    }
+
 
 }
---
+++ app/src/main/java/postApp/DataHandlers/MenuHandlers/FragmentHandlers/ExternalSystems/ShoppingList.java
@@ -0,0 +1,34 @@
+package postApp.DataHandlers.MenuHandlers.FragmentHandlers.ExternalSystems;
+
+import java.util.ArrayList;
+
+
+public class ShoppingList {
+    String listTitle;
+    ArrayList<String> itemList;
+    String clientID;
+
+    public ShoppingList(String listTitle, ArrayList<String> itemList, String clientID){
+        this.listTitle = listTitle;
+        this.itemList = itemList;
+        this.clientID = clientID;
+        itemList = new ArrayList<String>();
+    }
+
+    public String getClientID(){
+        return this.clientID;
+    }
+    public String getListTitle(){
+        return this.listTitle;
+    }
+    public ArrayList<String> getItemList(){
+        return this.itemList;
+    }
+
+    public void setClientID(String clientID){
+        this.clientID = clientID;
+    }
+    public void setListTitle(String listTitle){
+        this.listTitle = listTitle;
+    }
+}
