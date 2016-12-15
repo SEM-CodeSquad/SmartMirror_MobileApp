cc9c7585e2cb26e77ca82d2521664392925174f0
nimishkr <nimish97@yahoo.co.in>
2016-12-15 14:31:10 +0100

    Mirror Shopping list is only updated if phone shopping list is updated.

--- app/src/main/java/postApp/ActivitiesView/MenuView/FragmentViews/ExternalSystem/ShoppingView.java
+++ app/src/main/java/postApp/ActivitiesView/MenuView/FragmentViews/ExternalSystem/ShoppingView.java
@@ -1,34 +1,18 @@
-/*
- * Copyright 2016 CodeHigh
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Copyright (C) 2016 CodeHigh.
- *     Permission is granted to copy, distribute and/or modify this document
- *     under the terms of the GNU Free Documentation License, Version 1.3
- *     or any later version published by the Free Software Foundation;
- *     with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
- *     A copy of the license is included in the section entitled "GNU
- *     Free Documentation License".
- */
-
 package postApp.ActivitiesView.MenuView.FragmentViews.ExternalSystem;
 
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -41,15 +25,22 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.CountDownLatch;
+
+
 import adin.postApp.R;
 import postApp.ActivitiesView.MenuView.NavigationActivity;
 import postApp.Presenters.MenuPresenters.FragmentPresenters.ExternalSystems.ShoppingPresenter;
 
+import static android.R.id.message;
+
 /*
  * This class here initiates the shopping.xml which is the fragment view for the Shopping List
  * RFC that we are implementing. For consistency purposes, the list that holds the shopping
- * list items will be mentioned as 'shopping list' whereas references to the ShoppingList object
- * will be mentioned as 'ShoppingList Obj'
+ * list items will be mentioned as 'shopping list'.
  */
 public class ShoppingView extends Fragment {
     View myView;
@@ -60,12 +51,17 @@ public class ShoppingView extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        this.uuid = ((NavigationActivity) getActivity()).getMirror(); // TODO Use this method and pass it on.
+        this.uuid = ((NavigationActivity) getActivity()).getMirror();
         presenter = new ShoppingPresenter(this,uuid);
         setHasOptionsMenu(true);
         myView = inflater.inflate(R.layout.shopping, container, false);
         adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, presenter.getShoppingList());
         listView = (ListView) myView.findViewById(R.id.listView);
+
+        /*
+         * The below sleep method is required for the list to be properly fetched otherwise the view is updated
+         * before the fetch method.
+         */
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
@@ -88,13 +84,6 @@ public class ShoppingView extends Fragment {
             }
         });
 
-        /*
-         * The purpose of having a floating button is that the user will use this button to publish
-         * a new shopping list. Ater tapping the floating button, it will publish the list items along
-         * with the list title and will disappear. The title shall be saved internally and will remain
-         * uneditable as the RFC doesnt state a way to modify/fetch the title of a shopping list.
-         * This button will reappear upon deletion of the already created shopping list.
-         */
         return myView;
     }
     @Override
@@ -120,6 +109,10 @@ public class ShoppingView extends Fragment {
      */
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+
+        /*
+         * Menu button to add an item to the list.
+         */
         if (id == R.id.action_add) {
             if (uuid == "No mirror chosen") {
                 makeToast("Please choose a mirror first");
@@ -132,6 +125,14 @@ public class ShoppingView extends Fragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         presenter.updateList("add", input.getText().toString());
+
+                        /*
+                         * The below method stops the thread for 5 seconds but at interval of each half
+                         * a second, checks whether the phone has received a "done" message after
+                         * adding an element to the list. If it does receive the "done" reply,
+                         * the method then updates the list view adapter to display the added
+                         * item along with the original list.
+                         */
                         for (int i = 0; i < 10; i++) {
                             try {
                                 Thread.sleep(500);
@@ -166,6 +167,9 @@ public class ShoppingView extends Fragment {
             }
         }
 
+        /*
+         * Menu button to clear all list items.
+         */
         if (id == R.id.action_clear) {
             if (uuid == "No mirror chosen") {
                 makeToast("Please choose a mirror first");
@@ -176,6 +180,11 @@ public class ShoppingView extends Fragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         presenter.updateList("delete-list", "empty");
+
+                        /*
+                         * This method here is used in the same way as described in the previous comment,
+                         * except the fact that this is used to clear all the items in a list.
+                         */
                         for (int i = 0; i < 10; i++) {
                             try {
                                 Thread.sleep(500);
@@ -221,6 +230,11 @@ public class ShoppingView extends Fragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 presenter.updateList("delete", selectedItem);
+
+                /*
+                 * This method here is used in the same way as described in the previous comment,
+                 * except the fact that this is used to clear an item from the list.
+                 */
                 for (int i=0; i<10; i++){
                     try {
                         Thread.sleep(500);
@@ -250,15 +264,12 @@ public class ShoppingView extends Fragment {
         });
         builder.show();
     }
+
+    /*
+     * Method used to display toast messages.
+     */
     public void makeToast(String message){
         Toast.makeText(getActivity().getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
-
-    public ListView getListView(){
-        return this.listView;
-    }
-    public ArrayAdapter getAdapter(){
-        return this.adapter;
     }
 
 
